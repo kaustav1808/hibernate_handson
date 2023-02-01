8f34a2df81352d98b51dac1260ec8587767aee44
KAUSTAV BHATTACHARYA <kaustavofficial1808@gmail.com>
2023-02-01 19:45:34 +0530

    eager and lazy loading

---
+++ src/com/kaustav/hibernate/demo/EagerLazyLoadingDemo.java
@@ -0,0 +1,43 @@
+package com.kaustav.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kaustav.hibernate.demo.entity.Course;
+import com.kaustav.hibernate.demo.entity.Instructor;
+import com.kaustav.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyLoadingDemo {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+			
+			InstructorDetail detail = session.get(InstructorDetail.class, 1 );
+			
+			System.out.println(detail);
+			
+			System.out.println(detail.getInstructor());
+			
+			System.out.println(detail.getInstructor().getCourses());
+
+			session.getTransaction().commit();
+
+			System.out.println("Success.");
+		}finally {
+			factory.close();
+		}
+
+	}
+
+}
--- src/com/kaustav/hibernate/demo/entity/Instructor.java
+++ src/com/kaustav/hibernate/demo/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -49,7 +50,7 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructor_detail;
 	
-	@OneToMany(mappedBy="instructor", cascade= {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+	@OneToMany(fetch=FetchType.EAGER,mappedBy="instructor", cascade= {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
 	private List<Course> courses;
 
 	public InstructorDetail getInstructor_detail() {
